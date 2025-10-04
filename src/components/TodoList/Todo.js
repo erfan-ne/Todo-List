9890db5664b7c3052213bcdbae16eeb904eed61a
erfan-ne <erfan.neshati@gmail.com>
2025-10-04 12:53:09 +0330

    refactor(Todo.js): conversion clsses to functions

--- src/components/TodoList/Todo.js
+++ src/components/TodoList/Todo.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Todo extends Component {
+export default function Todo (props) {
 
-    checkBtnHandler(){
-        this.props.onCheckHandler(this.props.id)
+    const checkBtnHandler = () => {
+        props.onCheckHandler(props.id)
     }
 
-    trashBtnHandler(){
-        this.props.onTrashHandler(this.props.id)
+    const trashBtnHandler = () => {
+        props.onTrashHandler(props.id)
     }
 
-    render() {
-        return (
-            // 'completed' class for completed todos
-            <div className='todo' style={{ display: 'flex' }}>
-                <li className={`todo-item ${this.props.isComplete ? "completed" : ""}`}>
-                    {this.props.title}
-                </li>
-
-                <button className="check-btn" onClick={this.checkBtnHandler.bind(this)}>
-                    <i className="fas fa-check" aria-hidden="true"></i>
-                </button>
-
-                <button className="trash-btn" onClick={this.trashBtnHandler.bind(this)}>
-                    <i className="fas fa-trash" aria-hidden="true"></i>
-                </button>
-            </div>
-        )
-    }
+    return (
+        // 'completed' class for completed todos
+        <div className='todo' style={{ display: 'flex' }}>
+            <li className={`todo-item ${props.isComplete ? "completed" : ""}`}>
+                {props.title}
+            </li>
+            <button className="check-btn" onClick={checkBtnHandler}>
+                <i className="fas fa-check" aria-hidden="true"></i>
+            </button>
+            <button className="trash-btn" onClick={trashBtnHandler}>
+                <i className="fas fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+    )
 }
